65cbb7adfda2e3e1e803073444c7c215b7617845
Selena Siri <selenasiri@gmail.com>
2020-11-10 08:45:49 -0500

    icon button in sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,6 +1,9 @@
 import { Avatar } from '@material-ui/core'
 import React from 'react'
 import "./Sidebar.css";
+import SearchIcon from '@material-ui/icons/Search'
+import RateReviewOutlinedIcon from '@material-ui/icons/RateReviewOutlined';
+import IconButton from '@material-ui/core/IconButton'
 
 const Sidebar = () => {
   return (
@@ -8,6 +11,14 @@ const Sidebar = () => {
       <h2> I am the Sidebar</h2>
       <div className='sidebar_header'>
         <Avatar />
+        <div className='sidebar_input'>
+          <SearchIcon />
+          <input placeholder='Search' />
+        </div>
+
+        <IconButton variant='outlined' className='sidebar_inputButton'>
+          <RateReviewOutlinedIcon />
+        </IconButton>
       </div>
 
       <div className='sidebar_chats'>
